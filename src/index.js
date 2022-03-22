e511352bce3dc6f288d1e207c5ab488a93642d13
Solomon Achilles <solohag92@gmail.com>
2022-03-22 20:55:51 +0000

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -5,17 +5,17 @@ import refreshIcon from './Refresh_icon.svg';
 
 const toDoLists = [
   {
-    description: "Have my breakfast",
+    description: 'Have my breakfast',
     completed: false,
     index: 1,
   },
   {
-    description: "Take my lunch",
+    description: 'Take my lunch',
     completed: false,
     index: 2,
   },
   {
-    description: "Visit my mum",
+    description: 'Visit my mum',
     completed: false,
     index: 3,
   },
@@ -26,20 +26,19 @@ const listItems = document.querySelector('.list-items');
 const refreshImg = document.querySelector('.refresh-icon');
 refreshImg.src = refreshIcon;
 
-const enterImg =  document.querySelector('.enter-icon');
+const enterImg = document.querySelector('.enter-icon');
 enterImg.src = enterIcon;
 
 const populateLists = () => {
   toDoLists.forEach((list) => {
-    let listItem = document.createElement('li');
+    const listItem = document.createElement('li');
     listItem.className = 'list-item';
-    listItem.innerHTML = 
-    ` <div>
+    listItem.innerHTML = ` <div>
         <input type="checkbox"></input>
         <p>${list.description}</p>
       </div>
       <img src="${kebabMenu}" alt="Kebab-menu" class="kebab-icon">
-    `
+    `;
     // let radioBox = document.createElement('input');
     // radioBox.type = 'checkbox';
     // listItem.appendChild(radioBox);
@@ -47,9 +46,8 @@ const populateLists = () => {
     // ;
     listItems.appendChild(listItem);
   });
-}
+};
 
 document.addEventListener('DOMContentLoaded', () => {
   populateLists();
 });
-
