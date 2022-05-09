37ddfcea9494dc18de576492adf05547ddc6f066
Solomon Achilles <solohag92@gmail.com>
2022-05-09 10:15:21 +0000

    Add featured image

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,10 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
